6f4d2d8efc7a45e290441f04628befb5b87af6c1
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-08 14:10:53 -0400

    update switch to change dynamically

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,10 +6,16 @@ export class ThemeProvider extends Component {
   constructor(props) {
     super(props);
     this.state = { isDarkMode: false };
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
   }
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
