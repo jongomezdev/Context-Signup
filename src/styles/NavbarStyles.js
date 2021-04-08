ebefd2df79255f8aaff9029ac3c90aac2195d9e4
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-07 21:23:35 -0400

    style the navbar with MaterialUI, set up for dark and light mode

---
+++ src/styles/NavbarStyles.js
@@ -0,0 +1,44 @@
+import { fade } from '@material-ui/core/styles/colorManipulator';
+const styles = (theme) => ({
+  root: {
+    width: '100%',
+    marginBottom: 0,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
